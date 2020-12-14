6ac25c68af8220a40f099d8208bf7ed847299e05
jack <4396777@qq.com>
2020-12-14 09:15:35 +0800

    v2.0 order

--- admin/src/pages/order/detail.js
+++ admin/src/pages/order/detail.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
+import { Layout, Breadcrumb, Button } from 'antd';
+
+import { Link } from 'react-router-dom'
+
+const { Content } = Layout;
+import CustomLayout from 'components/custom-layout'
 
 class OrderDetail extends Component {
-
     render() {
         return (
             <div className="orderDetail">
-                this is order detail page
-            </div>
+                <CustomLayout>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>首页</Breadcrumb.Item>
+                        <Breadcrumb.Item>订单管理</Breadcrumb.Item>
+                        <Breadcrumb.Item>订单详情</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <Content
+                        className="site-layout-background"
+                        style={{
+                            padding: 24,
+                            margin: 0,
+                            minHeight: 280,
+                        }}
+                    >
+                        <div style={{fontSize:24}}>hello,this is orderList detail page</div>
+                    </Content>
+                </CustomLayout>
+            </div >
         )
     }
-
 }
-
 export default OrderDetail
--- admin/src/pages/order/list.js
+++ admin/src/pages/order/list.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react'
+import { Layout, Breadcrumb, Button } from 'antd';
+
+import { Link } from 'react-router-dom'
+
+const { Content } = Layout;
+import CustomLayout from 'components/custom-layout'
 
 class OrderList extends Component {
-
     render() {
         return (
-            <div className="orderList">
-                this is order list page
-            </div>
+            <div className="ad-list">
+                <CustomLayout>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>首页</Breadcrumb.Item>
+                        <Breadcrumb.Item>订单管理</Breadcrumb.Item>
+                        <Breadcrumb.Item>订单列表</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <Content
+                        className="site-layout-background"
+                        style={{
+                            padding: 24,
+                            margin: 0,
+                            minHeight: 280,
+                        }}
+                    >
+                        <div style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            marginBottom: "20px"
+                        }}>
+                            <div style={{ fontSize: 24 }}>hello,this is orderList page</div>
+                            <Link to='/order/detail'>
+                                <Button type="primary">详情</Button>
+                            </Link>
+                        </div>
+
+                    </Content>
+                </CustomLayout>
+            </div >
         )
     }
-
 }
-
 export default OrderList
